f0f47dc5cb14bc1717da6625fd5995655a0c6275
Rose389 <seifert.marguerite@yahoo.com>
2017-10-15 15:24:01 -0500

    It mostly works now

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,8 +6,9 @@ var connection = require("./connection")
 
 // data variables
 var stock = 0;
-var orderID = 0;
-var orderQty = 0;
+var price = 0;
+var prod = 0;
+var qty = 0;
 
 // the basic code
 connection.connect(function(err){
@@ -22,12 +23,11 @@ connection.connect(function(err){
 // display all products for sale
 	// ids - names - prices
 function afterConnection(){
-	var sql = "SELECT item_id, product_name, stock_quantity FROM products";
+	var sql = 'SELECT item_id, product_name, stock_quantity FROM products';
 
 	connection.query(sql, function(err, result){
-			stock = result.stock_quantity;
 			console.table(result);
-			userInput(stock);
+			userInput();
 
 	});
 }; // THIS PART WORKS
@@ -50,59 +50,43 @@ function userInput(){
 			}
 		])
 		.then(function(order) {
-			orderID = parseInt(order.item_ID);
-			orderQty = parseInt(order.quantity);
-
-			console.log("You have ordered " + orderQty + " units of item # " + orderID + ".");
-			checkAvail(orderID, orderQty);
+			checkAvail(order, stock);
 		});
 }
 
 
 // check if available quantity can fulfill order
-function checkAvail(order){
-	var sql = "SELECT stock_quantity FROM products WHERE item_id = " + orderID;
-	connection.query(sql, function (err, result, order) {
-		stock = parseInt(result.stock_quantity);
-		console.log(stock);
-
-		console.log(result.stock_quantity);
-	    if (err) throw err;
+function checkAvail(order, stock){
+	prod = order.item_ID;
+	qty = order.quantity;
+	console.log("id: " + prod + ", qty: " + qty + ", inv: " + stock);
 
 		// if no, let customer know
-		if (stock < orderQty){
-		console.log("We are unable to fulfill your order at this time.")
+		if (stock < qty){
+		console.log("We are unable to fulfill your order at this time.");
 
 		// and prevent order from completing
 		connection.end();
 
 		} else {
-		updateProduct(stock, orderID, orderQty);		
+		updateProduct(stock, order);		
 		}
-	});
 }
 
 
 // else if yes, update the SQL database
-function updateProduct(stock, orderID, orderQty){
-	// update query variable to retrieve id, price, and qty from database
-	var sql = "SELECT item_id, price, stock_quantity FROM products";
-	var query = connection.query(sql, function(err, result){
-		// set updated qty for stock
-		stock = stock - orderQty;
-		price = result.price;
-		// update query variable to update product quantity, nee to pass in orderQty?
-		sql = "UPDATE products SET stock_quantity = " + stock + " WHERE item_ID = " + orderID;
-		var query = connection.query(sql, function(err, result, orderQty, price){
-			// show records updated
-	    	// console.log("\n" + result.affectedRows + " record(s) updated\n");
-		
-			// and show customer the total cost of their purchase 
-			console.log("Your order total is: " + (orderQty * price));
+function updateProduct(stock, order){
+	console.log("Updating product " + order.item_ID);
 
-			// end connection
-			connection.end();
-		});
+	// update query variable to update product quantity,  to pass in orderQty?
+	sql = 'UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_ID = ?';
+	var query = connection.query(sql, [order.item_ID, order.quantity], function(err, result){
+		// show records updated
+		console.log("New quantity: " + (stock - order.quantity));
+		// and show customer the total cost of their purchase 
+		console.log("Your order total is: " + (order.quantity * price));
+
+		// end connection
+		connection.end();
 	});
-
 }
